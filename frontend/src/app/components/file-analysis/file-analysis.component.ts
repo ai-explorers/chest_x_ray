a6888a73ffc80d29bd2bb14d78bedd24f28b0aa5
Jan Raber <jan.raber@gmx.net>
2020-10-27 14:43:20 +0100

    Optimize http call order

--- frontend/src/app/components/file-analysis/file-analysis.component.ts
+++ frontend/src/app/components/file-analysis/file-analysis.component.ts
@@ -3,6 +3,7 @@ import { HttpService } from "../../services/http.service";
 import { ConversionService } from "../../services/conversion.service";
 import { SafeUrl } from "@angular/platform-browser";
 import { tap, concatMap, mergeMap } from "rxjs/operators";
+import { forkJoin } from 'rxjs';
 
 @Component({
   selector: 'app-file-analysis',
@@ -29,43 +30,39 @@ export class FileAnalysisComponent implements OnInit {
 
   handleFileUpload(files: FileList) {
     this.httpService.lungSegmentation(files.item(0))
-      .pipe(
-        tap((res: any) => {
-          console.log("Stage1: Lung segmentation was successful.");
-          this.segmentationResults.push({
-            title: files.item(0).name,
-            url: this.conversionService.arrayBufferToUrlString(res),
-            urlOriginal: this.conversionService.fileToUrlString(files.item(0)),
-            pneumoniaResult: null,
-            pneumoniaPredictionValue: null,
-            viralResult: null,
-            viralPredictionValue: null
-          });
-        }),
-        // using consecutive http calls, since they are depending on each other
-        concatMap((res: ArrayBuffer) =>
-          this.httpService.pneumoniaClassification(this.conversionService.arrayBufferToFile(res))
-        ),
-        tap((res: any) => {
-          console.log("Stage2: Pneumonia classification was successful.");
-          console.log("Result and prediction value: " + res.result + ", " + res.prediction_value);
-          let currentResult = this.segmentationResults.pop();
-          currentResult.pneumoniaResult = res.result;
-          currentResult.pneumoniaPredictionValue = Math.round(res.prediction_value * 100);
-          this.segmentationResults.push(currentResult);
-        }),
-        // using parallel http call, since you can execute both classifications independently
-        mergeMap((res: ArrayBuffer) =>
-          this.httpService.viralClassification(this.conversionService.arrayBufferToFile(res))
-        ),
-      )
-      .subscribe((res: any) => {
-        console.log("Stage3: Viral classification was successful.");
-        console.log("Result and prediction value: " + res.result + ", " + res.prediction_value);
-        let currentResult = this.segmentationResults.pop();
-        currentResult.viralResult = res.result;
-        currentResult.viralPredictionValue = Math.round(res.prediction_value * 100);
-        this.segmentationResults.push(currentResult);
+    .subscribe((segmentationRes: ArrayBuffer) => {
+      // display stage1 result
+      console.log("Stage1: Lung segmentation was successful.");
+      this.segmentationResults.push({
+        title: files.item(0).name,
+        url: this.conversionService.arrayBufferToUrlString(segmentationRes),
+        urlOriginal: this.conversionService.fileToUrlString(files.item(0)),
+        pneumoniaResult: null,
+        pneumoniaPredictionValue: null,
+        viralResult: null,
+        viralPredictionValue: null
       });
+      // simultaneous http calls for pneumonia and viral classification
+      // TODO: forkJoin only returns if both calls return! If stage2 or stage3 is unavailable, no result gets displayed! Use a mechanism like mergeMap here, so it's okay if only one call returns.
+      forkJoin([
+        this.httpService.pneumoniaClassification(this.conversionService.arrayBufferToFile(segmentationRes)),
+        this.httpService.viralClassification(this.conversionService.arrayBufferToFile(segmentationRes)),
+      ])
+      .subscribe(([pneumoniaRes, viralRes]: [any, any]) => {
+        console.log("Stage2&3: Pneumonia and viral classification was successful.");
+        console.log("Pneumonia result and prediction value: " + pneumoniaRes.result + ", " + pneumoniaRes.prediction_value);
+        console.log("Viral result and prediction value: " + viralRes.result + ", " + viralRes.prediction_value);
+        let currentResult = this.segmentationResults.pop();
+        currentResult.pneumoniaResult = pneumoniaRes.result;
+        currentResult.pneumoniaPredictionValue = Math.round(pneumoniaRes.prediction_value * 100);
+        currentResult.viralResult = viralRes.result;
+        currentResult.viralPredictionValue = Math.round(viralRes.prediction_value * 100);
+        this.segmentationResults.push(currentResult);
+      },
+      err => {
+        console.log("Error during pneumonia and viral classification!");
+        console.log(err);
+      });
+    });
   }
 }
